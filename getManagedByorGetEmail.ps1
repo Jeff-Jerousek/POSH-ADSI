1f37c27ad45465b7fcec14af7d6a1737ec87d6a8
Jeff Jerousek <jeff.jerousek@gmail.com>
2017-02-17 09:13:23 -0600

    Add files via upload

---
+++ getManagedByorGetEmail.ps1
@@ -0,0 +1,30 @@
+﻿function get-ManagedBy {
+#Get Domain
+$domain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+$root = $domain.GetDirectoryEntry()
+
+#Find computer in Domain
+$search = [System.DirectoryServices.DirectorySearcher]$root
+$search.Filter = "(&(cn=$env:COMPUTERNAME)(objectcategory=Computer))"
+$result = $search.FindOne()
+$computer = [ADSI]$result.path
+
+#write new description on Object
+$computer.managedBy
+
+}
+
+function get-email ($dn) {
+
+#Get Domain
+$domain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+$root = $domain.GetDirectoryEntry()
+
+#Find computer in Domain
+$search = [System.DirectoryServices.DirectorySearcher]$root
+$search.Filter = "(&(distinguishedname=$dn)(objectcategory=Person))"
+$result = $search.FindOne()
+$user = [ADSI]$result.path
+
+$user.mail
+}
